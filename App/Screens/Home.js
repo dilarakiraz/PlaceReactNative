68d9b7a0a563b1ed152560338fc610340bc1c20a
dilarakiraz <dilarakiraz12345@gmail.com>
2023-08-13 14:24:18 +0300

    google apı key

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../Components/Home/Header'
+import GoogleMapView from '../Components/Home/GoogleMapView'
 
+
+//API=  AIzaSyDALHJ2wczms-qP7NbN6RrDtoN6g9XHo58
 export default function Home() {
   return (
     <View style={{ padding:20 }}>
       <Header/>
+      <GoogleMapView/>
     </View>
   )
 }
