357c3cd74389f5f3fd28dc39e3160657851ccfb0
dilarakiraz <76565172+dilarakiraz@users.noreply.github.com>
2023-08-17 08:30:40 +0300

    Update GlobalApi.js

--- App/Services/GlobalApi.js
+++ App/Services/GlobalApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const BASE_URL = "https://maps.googleapis.com/maps/api/place"
-const API_KEY = "AIzaSyDALHJ2wczms-qP7NbN6RrDtoN6g9XHo58"
+const API_KEY = "{$API_KEY}"
 
 const nearByPlace = (lat, lng, type) => axios.get(BASE_URL +
     "/nearbysearch/json?" +
